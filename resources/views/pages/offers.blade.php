40361267d635f1ef2cc61908430a1561f8df4a18
Muhamad Amir <m.amirazmi@hotmail.com>
2020-04-25 07:09:50 +0800

    added all static pages

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -1,7 +1,53 @@
 @extends('layouts/app')
 
 @section('content')
-  <div class="">
-    <h1>Offers</h1>
-  </div>
+    <div id="offers-page">
+        <div class="content-box">
+            <div class="row">
+              <div class="col-md-8 offset-md-2">
+                <h1>Sign Up To Notifications For Deals</h1>
+                <form>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="firstnameinput">First Name</label>
+                      <input type="text" name="fname" class="form-control" id="firstnameinput" placeholder="John">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="lastnameinput">Last Name</label>
+                      <input type="lname" name="phone" class="form-control" id="lastnameinput" placeholder="Doe">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="emailinput">Email address</label>
+                      <input type="email" name="email" class="form-control" id="emailinput" placeholder="name@example.com">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="phoneinput">Phone #</label>
+                      <input type="text" name="phone" class="form-control" id="phoneinput" placeholder="718-219-8652">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <p>In signing up I acknowledge that I am 18 years of age or older, want to receive email offers from Billy's Burgers and, if I select to join Dine Rewards, agree to the terms and conditions of the program.</p>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <button type="submit" class="btn btn-primary">Confirm</button>
+                    </div>    
+                  </div>
+                </div>
+                </form>
+              </div>
+            </div>
+        </div>
+    </div>
 @endsection
