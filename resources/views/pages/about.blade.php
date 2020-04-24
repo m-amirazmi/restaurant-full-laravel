9a61f96e301b675a496269b832c9a1ea7bcc848b
Muhamad Amir <m.amirazmi@hotmail.com>
2020-04-25 03:26:08 +0800

    main layout responsive for mobile and tabs

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts/app')
 
 @section('content')
-  <div class="welcome-jumbo">
-    <h1>About</h1>
-  </div>
+    <div id="about-page">
+        <div class="content-box">
+            <div class="row">
+              <div class="col-md-6">
+                <h1>About</h1>
+                <p>I'm baby chartreuse iceland bitters jianbing tote bag pop-up farm-to-table literally. Readymade hashtag farm-to-table bicycle rights mustache. Authentic tofu 8-bit kinfolk banh mi vice. Glossier lo-fi raw denim pop-up, vape seitan gochujang cardigan taxidermy kale chips locavore roof party taiyaki selfies.</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, non quia? Eum ut vero, minima, voluptate tempore cupiditate debitis ex blanditiis nobis veritatis, a dolore? Exercitationem quae saepe autem sed asperiores magnam voluptatibus quos dolorem ducimus perferendis illo quasi suscipit eius id consectetur possimus veritatis, dignissimos nam accusantium a. Asperiores voluptas voluptatem, esse delectus aliquam recusandae dolore cum nesciunt minima officiis voluptates, quia reiciendis magnam quas similique deserunt. Cupiditate ut eos culpa, alias vero commodi vel vitae minus ratione et quisquam nulla dolore dolorum. Voluptatibus ea quo delectus, animi non culpa consequuntur cupiditate laborum eius facere magni illo, consectetur consequatur!</p>
+              </div>
+              <div class="col-md-6">
+                <img src="img/aboutpageimg.jpg" class="img-fluid" alt="">
+              </div>
+            </div>
+        </div>
+    </div>
 @endsection
