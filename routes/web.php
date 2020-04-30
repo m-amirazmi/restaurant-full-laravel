46d7c33646fd88c284ab754f5a2b911e4ebc0cfd
Muhamad Amir <m.amirazmi@hotmail.com>
2020-05-01 03:05:47 +0800

    created users migration, seeds, admin users can create new users

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::get('/admin/reservations','admin\CustomerController@allReservations');
 //Admin Users
 Route::get('/admin/users', 'admin\UsersController@index');
 Route::get('/admin/users/create','admin\UsersController@create');
+Route::post('/admin/users','admin\UsersController@store');
 Route::get('/admin/users/{id}/edit', 'admin\UsersController@edit');
 
 //Admin Auth
