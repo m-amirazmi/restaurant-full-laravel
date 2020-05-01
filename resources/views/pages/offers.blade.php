51028d6ece3bc76dcba33a19085cb4824aaba5bd
Muhamad Amir <m.amirazmi@hotmail.com>
2020-05-01 13:58:40 +0800

    created new member from front page

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -6,32 +6,53 @@
             <div class="row">
               <div class="col-md-8 offset-md-2">
                 <h1>Sign Up To Notifications For Deals</h1>
-                <form>
+                <form method="POST" action="/offers">
+                  @csrf
                 <div class="row">
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label for="firstnameinput">First Name</label>
-                      <input type="text" name="fname" class="form-control" id="firstnameinput" placeholder="John">
+                      <label for="inputfname">First Name</label>
+                      <input id="inputfname" type="text" class="form-control form-control-lg @error('fname') is-invalid @enderror" name="fname" value="{{ old('fname') }}" required autocomplete="fname" placeholder="John"  autofocus>
+                      @error('fname')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label for="lastnameinput">Last Name</label>
-                      <input type="lname" name="phone" class="form-control" id="lastnameinput" placeholder="Doe">
+                      <label for="inputlname">Last Name</label>
+                      <input id="inputlname" type="text" class="form-control form-control-lg @error('lname') is-invalid @enderror" name="lname" value="{{ old('lname') }}" required autocomplete="lname" placeholder="Doe"  autofocus>
+                      @error('lname')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
                     </div>
                   </div>
                 </div>
                 <div class="row">
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label for="emailinput">Email address</label>
-                      <input type="email" name="email" class="form-control" id="emailinput" placeholder="name@example.com">
+                      <label for="inputemail">Email address</label>
+                      <input id="inputemail" type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="name@example.com"  autofocus>
+                      @error('email')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="form-group">
-                      <label for="phoneinput">Phone #</label>
-                      <input type="text" name="phone" class="form-control" id="phoneinput" placeholder="718-219-8652">
+                      <label for="inputphone">Phone #</label>
+                      <input id="inputphone" type="tel" class="form-control form-control-lg @error('phone_number') is-invalid @enderror" name="phone_number" value="{{ old('phone_number') }}" required autocomplete="phone_number" placeholder="234-123-4242"  autofocus>
+                      @error('phone_number')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
                     </div>
                   </div>
                 </div>
